ade10e49231a6628131bfde5baed3b5101d8ca18
Marcelle Vargas <marcellevargas@Marcelle-Vargas-MacBook-Pro-464A.local>
2022-07-27 08:05:16 -0300

    Ajuste na tela principal

--- src/components/current-weather.jsx
+++ src/components/current-weather.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import weatherApi from "../services/weather-api";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 36px;
+  margin-bottom: 10px;
+`;
+
+const WeatherInfo = styled.div`
+  display: flex;
+  justify-content: space-around;
+  font-size: 20px;
+`;
 
 const CurrentWeather = () => {
+  const [weatherDay, setWeatherDay] = useState([]);
 
-  weatherApi
-    .get("/")
-    .then((res) => console.log(res.data))
-    .catch((err) => {
-      console.error("ops! ocorreu um erro" + err);
-    });
+  useEffect(() => {
+    weatherApi
+      .get("/")
+      .then((res) => {
+        let weather_data = res.data;
+        setWeatherDay(weather_data);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
 
   return (
     <>
-      <h1>Current Weather</h1>
+      <Title>Current Weather</Title>
+
+      <WeatherInfo>
+        <p>Temperatura {weatherDay?.tempoAtual}°</p>
+        <p>Chance de chuva {weatherDay?.chanceChuva}</p>
+        <p>Umidade {weatherDay?.umidade}</p>
+        <p>Vento {weatherDay?.vento}</p>
+      </WeatherInfo>
     </>
   );
 };
